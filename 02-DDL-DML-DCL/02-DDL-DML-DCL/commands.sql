bda195ab8d34beb629eab6a89f919452b1073322
ADIESHWAR REDDY MOGILI <mogiliadieshwarreddy@gmail.com>
2026-09-21 12:21:50 +0530

    Update commands.sql

--- 02-DDL-DML-DCL/02-DDL-DML-DCL/commands.sql
+++ 02-DDL-DML-DCL/02-DDL-DML-DCL/commands.sql
@@ -1,1 +1,34 @@
+DML - Data Manipulation Language
+
+DML commands modify data stored inside tables.
+
+Commands:
+
+INSERT
+UPDATE
+DELETE
+
+Example:
+
+INSERT INTO students
+VALUES (1, 'Adhi');
+DQL - Data Query Language
+
+SELECT is used to retrieve data.
+
+SELECT * FROM students;
+DCL - Data Control Language
+
+DCL controls database permissions.
+
+Commands:
+
+GRANT
+REVOKE
+Conclusion
+
+DDL defines database structure, DML modifies data and DCL manages permissions.
+
+
+---
 
