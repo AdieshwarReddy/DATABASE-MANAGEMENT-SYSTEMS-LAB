4b4762990b17fe671bce8ed97c51f2535e50b615
ADIESHWAR REDDY MOGILI <mogiliadieshwarreddy@gmail.com>
2026-09-21 14:47:53 +0530

    Add transaction and rollback for employee salary updates

--- 09-TCL-Commands/tcl.sql
+++ 09-TCL-Commands/tcl.sql
@@ -1,1 +1,16 @@
 
+START TRANSACTION;
+
+UPDATE employees
+SET salary = salary + 5000
+WHERE employee_id = 101;
+
+SAVEPOINT salary_update;
+
+UPDATE employees
+SET salary = salary + 10000
+WHERE employee_id = 102;
+
+ROLLBACK TO salary_update;
+
+COMMIT;
