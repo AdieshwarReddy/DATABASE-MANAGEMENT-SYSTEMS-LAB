f75aa05c5636f2863b2780360b3b42c63ff5ad87
ADIESHWAR REDDY MOGILI <mogiliadieshwarreddy@gmail.com>
2026-09-21 15:05:10 +0530

    Add salary audit table and update trigger
    Created a salary_audit table and a trigger to log salary changes.

--- 12-Views-Triggers/12-Views-Triggers/triggers.sql
+++ 12-Views-Triggers/12-Views-Triggers/triggers.sql
@@ -1,1 +1,54 @@
 
+CREATE TABLE salary_audit (
+
+    audit_id INT AUTO_INCREMENT PRIMARY KEY,
+
+    employee_id INT,
+
+    old_salary DECIMAL(10,2),
+
+    new_salary DECIMAL(10,2),
+
+    changed_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+
+);
+
+
+
+
+DELIMITER //
+
+CREATE TRIGGER salary_update_trigger
+
+AFTER UPDATE ON employees
+
+FOR EACH ROW
+
+BEGIN
+
+    IF OLD.salary <> NEW.salary THEN
+
+        INSERT INTO salary_audit(
+            employee_id,
+            old_salary,
+            new_salary
+        )
+
+        VALUES(
+            OLD.employee_id,
+            OLD.salary,
+            NEW.salary
+        );
+
+    END IF;
+
+END //
+
+
+
+  UPDATE employees
+SET salary = 70000
+WHERE employee_id = 101;
+
+SELECT * FROM salary_audit;
+DELIMITER ;
