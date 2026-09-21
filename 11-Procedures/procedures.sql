881be396065dcc49a7613cbc721841a1248273ad
ADIESHWAR REDDY MOGILI <mogiliadieshwarreddy@gmail.com>
2026-09-21 14:59:01 +0530

    Update procedures.sql

--- 11-Procedures/procedures.sql
+++ 11-Procedures/procedures.sql
@@ -1,1 +1,28 @@
+DELIMITER //
 
+CREATE PROCEDURE GetEmployees()
+BEGIN
+    SELECT * FROM employees;
+END //
+
+DELIMITER ;
+
+CALL GetEmployees();
+
+
+DELIMITER //
+
+CREATE PROCEDURE GetEmployeesByCity(
+    IN city_name VARCHAR(100)
+)
+BEGIN
+
+    SELECT *
+    FROM employees
+    WHERE city = city_name;
+
+END //
+
+DELIMITER ;
+
+CALL GetEmployeesByCity('Hyderabad');
