a5d29868afa06329768cb3ce3e529a0a8d0f5da0
ADIESHWAR REDDY MOGILI <mogiliadieshwarreddy@gmail.com>
2026-09-21 14:42:56 +0530

    Add various SQL subqueries for employee data

--- 07-Subqueries/subqueries.sql
+++ 07-Subqueries/subqueries.sql
@@ -1,1 +1,52 @@
+SELECT employee_name, salary
+FROM employees
+WHERE salary >
+(
+    SELECT AVG(salary)
+    FROM employees
+);
 
+SELECT employee_name
+FROM employees
+WHERE department_id IN
+(
+    SELECT department_id
+    FROM departments
+    WHERE department_name IN ('IT', 'HR')
+);
+
+SELECT employee_name, salary
+FROM employees
+WHERE salary > ALL
+(
+    SELECT salary
+    FROM employees
+    WHERE department_id = 2
+);
+
+SELECT employee_name, salary
+FROM employees
+WHERE salary > ANY
+(
+    SELECT salary
+    FROM employees
+    WHERE department_id = 1
+);
+
+SELECT department_name
+FROM departments d
+WHERE EXISTS
+(
+    SELECT 1
+    FROM employees e
+    WHERE e.department_id = d.department_id
+);
+
+SELECT department_name
+FROM departments d
+WHERE NOT EXISTS
+(
+    SELECT 1
+    FROM employees e
+    WHERE e.department_id = d.department_id
+);
